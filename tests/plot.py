fixed the origin distancre scaling

tests/plot.py
@@ -3,13 +3,15 @@ Plots the results of our data tests
 """
 import matplotlib.pyplot as plt
 import numpy as np
-plt.figure()
+#plt.figure()
 
-COMPARE = False# this is for the comparison of the two methods
-NN = True
+COMPARE = True# this is for the comparison of the two methods
+NN = False
 EIG = False
-test_accs = np.genfromtxt("data/averaged_runs/SAGE.txt")
-test_accs_vanilla = np.genfromtxt("data/averaged_runs/NN.txt")
+f1 = "data/averaged_runs/GAT.txt"
+f2 = "data/averaged_runs/GIANT_GAT.txt"
+test_accs = np.genfromtxt(f1)
+test_accs_vanilla = np.genfromtxt(f2)
 #print(len(test_accs))
 print(test_accs.shape)
 #note put the NN and the eigenvector in test_accs_vanilla
@@ -18,9 +20,12 @@ z = test_accs[:,0] #accs
 z = z.reshape(200,61)
 x = x.reshape(200,61)
 y = test_accs[:,2]# mu
+y = y
 first_z = z
 #y = y.reshape(200,61)
 print(test_accs_vanilla.shape)
+f1 = f1[19:]
+f2 = f2[19:]
 
 if COMPARE:
     new_Z = np.zeros((200,61))
@@ -51,33 +56,32 @@ if COMPARE:
         comp = ax[0].scatter(x,y,c=z,cmap="coolwarm",vmin = -.4,vmax=.4)
         plt.colorbar(comp,ax=ax[0])
         ax[2].scatter(test_accs_vanilla[:,2],test_accs_vanilla[:,0])
-        normal = ax[1].scatter(x,y,c=first_z,cmap="coolwarm",vmin=.5,vmax=1)
+        normal = ax[1].scatter(x,y,c=first_z,cmap="coolwarm")
         plt.colorbar(normal, ax=ax[1])
         ax[0].set_title("Comparison of Two")
-        ax[1].set_title("Acc of GCN/GAT/SAGE")
-        ax[2].set_title("Acc of NN")
+        ax[1].set_title(f"Acc of {f1}")
+        ax[2].set_title(f"Acc of {f2}")
     elif EIG:
-        comp = ax[1].scatter(x,y,c=first_z,cmap="coolwarm",vmin=.5,vmax=1)
+        comp = ax[1].scatter(x,y,c=first_z,cmap="coolwarm")
         comp_color = plt.colorbar(comp,ax = ax[1])
         normal = ax[0].scatter(x,y,c=z,cmap="coolwarm",vmin=-.4,vmax = .4)
         plt.colorbar(normal,ax=ax[0])
         ax[2].scatter(test_accs_vanilla[:,1],test_accs_vanilla[:,0])
         ax[0].set_title("Comparison of Two")
-        ax[1].set_title("Acc of GCN/GAT/SAGE")
-        ax[2].set_title("Acc of Spectral")
+        ax[1].set_title(f"Acc of {f1}")
+        ax[2].set_title(f"Acc of {f2}")
     else:
-        comp = ax[1].scatter(x,y,c=first_z,cmap="coolwarm",vmin=.5,vmax=1)
+        comp = ax[1].scatter(x,y,c=first_z,cmap="coolwarm")
         comp_color = plt.colorbar(comp,ax = ax[1])
         normal = ax[0].scatter(x,y,c=z,cmap="coolwarm",vmin=-.4,vmax = .4)
         plt.colorbar(normal,ax=ax[0])
-        other = ax[2].scatter(test_accs_vanilla[:,:,1],test_accs_vanilla[:,:,2],c=test_accs_vanilla[:,:,0],cmap="coolwarm",vmin = .5,vmax = 1)
+        other = ax[2].scatter(test_accs_vanilla[:,:,1],test_accs_vanilla[:,:,2],c=test_accs_vanilla[:,:,0],cmap="coolwarm")
         plt.colorbar(other,ax=ax[2])
         ax[0].set_title("Comparison of Two")
-        ax[1].set_title("Acc of GCN/GAT/SAGE")
-        ax[2].set_title("Acc of Second GCN/GAT/SAGE")
-    
+        ax[1].set_title(f"Acc of {f1}")
+        ax[2].set_title(f"Acc of {f2}")
 else:
-    plt.scatter(x,y,c=z,cmap="coolwarm")
+    plt.scatter(x,y,c=z,cmap="coolwarm",vmin = 0,vmax=1)
     plt.colorbar()
     plt.xlabel("edge info - interpolation between random and planted")
     # normalized degree separation, lambda