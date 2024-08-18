379efc08e8b6c3fb6481ba17296f983cf4a86499
Kaden Parker <bobert11@login01.rc.byu.edu>
2024-08-18 11:46:28 -0600

    To run script saving models run job2.sh which will run generate_data_organized2.py I changed a few things in that script to have it iterate over lambda values.

--- generate_data_organized2.py
+++ generate_data_organized2.py
@@ -8,7 +8,7 @@ import argparse
 import matplotlib.pyplot as plt
 import seaborn as sns
 import torch.nn.functional as F
-from generate_data2 import *
+# from generate_data2 import *
 from src.models import *
 from src.utils import *
 from torch_geometric.data import Data
@@ -79,6 +79,7 @@ def train_model(model, optimizer, dataset):
         train_loss = F.nll_loss(out, train_labels)
         train_loss.backward()
         optimizer.step()
+    torch.save(model.state_dict(),f"trained_models/model_with{mu}mu_{lamb}lamb")
 
 def train_transformer_model(model,optimizer,dataset):
     train_edge_list, train_features, train_labels = dataset
@@ -97,6 +98,8 @@ def train_transformer_model(model,optimizer,dataset):
             train_loss = F.cross_entropy(out, data.y.squeeze())
             train_loss.backward()
             optimizer.step()
+    torch.save(model.state_dict(),f"trained_models/model_with{mu}mu_{lamb}lamb")
+
 def get_data(num_nodes, num_features, average_degree, mu, lamb, degree_corrected, num_classes,ret_adj=False):
     return get_csbm(num_nodes, num_features, average_degree, mu, lamb, degree_corrected, num_classes,ret_adj=ret_adj),get_csbm(num_nodes, num_features, average_degree, mu, lamb, degree_corrected, num_classes,ret_adj=ret_adj)
 
@@ -115,13 +118,17 @@ def get_csbm(num_nodes, num_features, average_degree, mu, lamb, degree_corrected
     # Generate SBM graph
     sbm_graph = nx.stochastic_block_model(block_sizes, p_matrix).to_directed()
     if degree_corrected:
+        #Kaden added line 122-123
+        for node in sbm_graph.nodes():
+            sbm_graph.nodes[node]['degree']=1
+
         for i in range(num_classes):
             degrees = powerlaw.rvs(2, size=class_size).astype(int)
             for j, node in enumerate(range(i * class_size, (i + 1) * class_size)):
                 sbm_graph.nodes[node]['degree'] = degrees[j]
 
             # Rewire edges based on degrees
-            for u, v in sbm_graph.edges():
+            for u, v in list(sbm_graph.edges()):
                 u_degree = sbm_graph.nodes[u]['degree']
                 v_degree = sbm_graph.nodes[v]['degree']
                 p_rewire = u_degree * v_degree / (num_nodes * average_degree)
@@ -154,10 +161,10 @@ parser.add_argument("--average_degree", type=int,default=10, help= "This is the 
 parser.add_argument("--degree_corrected", type=bool,default=False, help= "This is whether or not we are calculating degree corrected graphs")
 parser.add_argument("--hidden_features", type=int,default=16, help= "This is whether or not we are calculating degree corrected graphs")
 parser.add_argument("--lr", type=float,default=.01, help= "The default learning rate")
-parser.add_argument("--runs", type=int,default=10, help= "The number of runs to max over")
+parser.add_argument("--runs", type=int,default=1, help= "The number of runs to max over")
 parser.add_argument("--epochs", type=int,default=400, help= "The number of epochs we are planning to run")
 parser.add_argument("--device", type=str,default="cuda", help= "The number of epochs we are planning to run")
-parser.add_argument('-models', nargs='+', help='The models you will be passing through', required=True)
+parser.add_argument('--models', nargs='+', help='The models you will be passing through')
 
 args = parser.parse_args()
 print(args)
@@ -165,21 +172,27 @@ mu_range = [args.comp_id/args.max_comps*args.mu_max]
 lambda_range = torch.linspace(args.lambda_min,args.lambda_max,121)
 models = [eval(m) for m in args.models]
 print(models)
+print("test")
 for model_type in models:
-    all_accs = []   
-    if model_type is NN:
+    print("starting")
+    all_accs = []  
+    print(f'model type is{model_type}') 
+    # if model_type is NN:
+    if model_type is GCN or GAT:
         for mu in mu_range:
-            accs = []
-            for run in range(args.runs):
-                train_dataset, test_dataset = get_data(num_nodes=args.num_nodes, num_features=args.num_features, 
-                                                        average_degree=args.average_degree, mu=mu, lamb=0, 
-                                                        degree_corrected=args.degree_corrected, num_classes=args.num_classes)
-                model, optimizer = get_model(model_type,args.num_features, args.hidden_features, args.num_classes)
-                model = model.to(args.device)
-                train_model(model,optimizer,train_dataset)
-                accs.append(accuracy_1(torch.argmax(model(test_dataset[1].to(args.device),test_dataset[0].to(args.device)),dim=-1),test_dataset[2].to(args.device)))
-            print(max(accs),0,mu)
-            store_acc(max(accs),0,mu,model_type,args.num_classes)
+            for lamb in lambda_range:
+                accs = []
+                for run in range(args.runs):
+                    train_dataset, test_dataset = get_data(num_nodes=args.num_nodes, num_features=args.num_features, 
+                                                            average_degree=args.average_degree, mu=mu, lamb=0, 
+                                                            degree_corrected=args.degree_corrected, num_classes=args.num_classes)
+                    model, optimizer = get_model(model_type,args.num_features, args.hidden_features, args.num_classes)
+                    model = model.to(args.device)
+                    train_model(model,optimizer,train_dataset)
+                    print("trained!")
+                    accs.append(accuracy_1(torch.argmax(model(test_dataset[1].to(args.device),test_dataset[0].to(args.device)),dim=-1),test_dataset[2].to(args.device)))
+                print(max(accs),lamb,mu)
+                store_acc(max(accs),0,mu,model_type,args.num_classes)
     if model_type is Spectral:
         for lamb in lambda_range:
             accs = []
--- job2.sh
+++ job2.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=50G   # memory per CPU core
 #SBATCH -J "GAT 2-class"   # job name
-#SBATCH --array=0-1
+#SBATCH --array=0-200
 #SBATCH --gpus=1
 ##SBATCH --mail-type=FAIL
 
@@ -15,4 +15,5 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE 
 mamba activate geo2
-python3 -u store_data.py $SLURM_ARRAY_TASK_ID 2 3 10 1000 10 2 "False" "True"
+# python3 -u generate_data_organized2.py $SLURM_ARRAY_TASK_ID 2 3 10 1000 10 2 "False" "True"
+python3 -u generate_data_organized2.py --comp_id $SLURM_ARRAY_TASK_ID --max_comps 200 --mu_max 2 --lambda_min -3 --lambda_max 3 --num_features 10 --num_nodes 1000 --num_classes 2 --average_degree 10 --degree_corrected "False" --hidden_features 16 --lr .01 --runs 1 --epochs 400 --device "cuda" --models GCN
